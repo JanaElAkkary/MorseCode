62d38a5b04a4de72ae549497e645f681c14a5201
JanaElAkkary <145542498+JanaElAkkary@users.noreply.github.com>
2023-11-28 00:54:43 +0200

    Update Jana:MorseCode.py

--- Jana:MorseCode.py
+++ Jana:MorseCode.py
@@ -35,8 +35,29 @@ def morse_decryptor(message):
         print("Not Defined, Please Enter text to decrypt using Morse code: ")
     return message_after_decryption 
 
+def morse_encrypter_arabic(message):
+    message_after_encryption_arabic = " "
+    morse_dict_3 ={'ا': '.-', 'ب': '-...', 'ث': '-.-.', 'د': '-..', 'ء': '.', 'ف': '..-.', 'غ': '--.', 'ح': '....',
+    'ي': '..', 'ج': '.---', 
+    'ك': '-.-', 'ل': '.-..', 'م': '--', 'ن': '-.', 'خ': '---',
+    'ق': '--.-', 'ر': '.-.', 'س': '...', 'ت': '-', 'ط': '..-', 'ض': '...-', 'و': '.--', 'ص': '-..-',
+    'ظ': '-.--', 'ذ': '--..','ز':'---.','ش':'----','ع':'.-.-','ه':'..-..'}
+    message_after_encryption = " ".join(morse_dict_3.get(character,'') for character in message.upper()) 
+    if message == "":
+        print("Not Defined, Please Enter text to encrypt using Morse code:  ")
+    return message_after_encryption_arabic
+
+
+
+
+
+
+
+
+
+
 while True:
-    choice = input("Choose 0 for exit, 1 for morse_encrypter, 2 for morse_decrypter: ")
+    choice = input("Choose 0 for exit, 1 for morse_encrypter, 2 for morse_decrypter, 3 for morse_ encrypter_arabic: ")
     if choice == '0':
         break
     elif choice == '1':
@@ -44,6 +65,9 @@ while True:
         print(morse_encrypter(message))
     elif choice == '2': 
         message = input("Enter messege to decrypt: ")
-        print(morse_decryptor(message))                          
+        print(morse_decryptor(message)) 
+    elif choice == '3':
+        message = input("Enter message to encrypt from arabic: ")  
+        print(morse_encrypter_arabic(message))                       
     else: 
-       print("Invalid input") 
+       print("Undifined input") 

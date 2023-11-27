94d1efa64f1d0ba71b4840111242afd003c1d949
JanaElAkkary <145542498+JanaElAkkary@users.noreply.github.com>
2023-11-27 22:13:47 +0200

    Update Jana:MorseCode.py

--- Jana:MorseCode.py
+++ Jana:MorseCode.py
@@ -1,20 +1,22 @@
 # morse code encrypter and decrypter.
+print("welcome to Morse Translater")
 print ("Enter text to encrypt using Morse code: ")
+
 def morse_encrypter(message):
     message_after_encryption = " "
     morse_dict_1 ={'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'I': '..', 'J': '.---', 
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
     'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
     'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
     '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
     ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
     '$': '...-..-', '@': '.--.-.', ' ': '/'}
-    message_after_encryption = "" .join (morse_dict_1.get(c,'') for c in message.upper()) 
+    message_after_encryption = " ".join(morse_dict_1.get(character,'') for character in message.upper()) 
     if message == "":
-        print("Enter text to encrypt using Morse code:  ")
+        print("Not Defined, Please Enter text to encrypt using Morse code:  ")
     return message_after_encryption
-
 
 print("Enter text to decrypt using Morse code: ")
 def morse_decryptor(message):
@@ -28,9 +30,9 @@ def morse_decryptor(message):
     '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&',
     '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_', '.-..-.': '"',
     '...-..-': '$', '.--.-.': '@', '/': ' '}
-    message_after_decryption="" .join (morse_dict_2.get (c,'')for c in message.split())
-    if message =="":
-        print("Enter text to decrypt using Morse code: ")
+    message_after_decryption="".join (morse_dict_2.get (character,'')for character in message.split())
+    if message== "":
+        print("Not Defined, Please Enter text to decrypt using Morse code: ")
     return message_after_decryption 
 
 while True:
@@ -45,5 +47,3 @@ while True:
         print(morse_decryptor(message))                          
     else: 
        print("Invalid input") 
-
-    
